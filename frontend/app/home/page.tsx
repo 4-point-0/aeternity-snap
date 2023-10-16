53032d0d2619d9b913f907655a77271eed2d823f
dbosnjak94 <dominik.bosnjak94@gmail.com>
2023-10-16 16:59:26 +0200

    removing unused elements

--- frontend/app/home/page.tsx
+++ frontend/app/home/page.tsx
@@ -1,15 +1,6 @@
 "use client";
 import React from "react";
-import {
-  Card,
-  CardHeader,
-  CardBody,
-  CardFooter,
-  Divider,
-  Link,
-  Image,
-  Button,
-} from "@nextui-org/react";
+import { Card, Button } from "@nextui-org/react";
 
 export const Home = () => {
   return (
