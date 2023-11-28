477727fc7cd9b1fd2a5e11e6eb6005e5505a184d
AndWa <andrej.saric.du@gmail.com>
2023-11-28 20:08:23 +0100

    Add 404 page, fix qrcode package

---
+++ packages/site/app/not-found.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+export default function NotFound() {
+  return (
+    <section>
+      <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+        <div className="mx-auto max-w-screen-sm text-center">
+          <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl">
+            404
+          </h1>
+          <p className="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">
+            Something's missing.
+          </p>
+          <p className="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">
+            Sorry, we can't find that page. You'll find lots to explore on the
+            home page.{" "}
+          </p>
+          <a
+            href="/"
+            className="inline-flex text-white focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center my-4"
+          >
+            Back to Homepage
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+}
