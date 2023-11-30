9b2ee9a67c40a39ae36b3935d992e15cb83608a5
AndWa <andrej.saric.du@gmail.com>
2023-11-30 11:57:59 +0100

    Escape single quote

--- packages/site/app/not-found.tsx
+++ packages/site/app/not-found.tsx
@@ -9,11 +9,11 @@ export default function NotFound() {
             404
           </h1>
           <p className="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">
-            Something's missing.
+            Something&lsquo;s missing.
           </p>
           <p className="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">
-            Sorry, we can't find that page. You'll find lots to explore on the
-            home page.{" "}
+            Sorry, we can&lsquo;t find that page. You'll find lots to explore on
+            the home page.{" "}
           </p>
           <a
             href="/"
